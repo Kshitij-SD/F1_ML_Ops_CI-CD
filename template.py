a8e07f408733dc7c3926e02a676c835201b8bab3
Kshitij-SD <kshitij9802@gmail.com>
2025-03-23 13:29:59 +0530

    "added logging and exceptions"

--- template.py
+++ template.py
@@ -8,7 +8,7 @@ logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 project_name = "F1_Stint_Prediction"
 
 list_of_files = [
-    ".github/workflows/.gitkeep"
+    ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
